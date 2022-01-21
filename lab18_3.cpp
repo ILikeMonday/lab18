7a29f192c4dfe795cf57e89530c0aaf69fc66f3c
ILikeMonday <worldrocker64@gmail.com>
2022-01-21 14:27:15 +0700

    all done

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -4,10 +4,27 @@
 #include<vector>
 using namespace std;
 
-vector<int> randomVector(int);
-void showVector(vector<int>);
-int dotProduct(vector<int>,vector<int>);
+vector<int> randomVector(unsigned int N){	
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
 
+void showVector(vector<int> v){
+	cout << "[";
+	for(unsigned int i = 0; i < v.size(); i++){
+		cout << v[i];
+		if(i==v.size()-1) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> k,vector<int> h){
+	int sum = 0;
+	for(unsigned int i = 0; i < h.size(); i++) sum += k[i]*h[i];	
+	return sum;
+
+}
 int main(){
 	srand(time(0));
 	vector<int> x = randomVector(5);
